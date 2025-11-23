129a9692627dacd3076c2ea8e47d7613acd426af
p-x9 <p.x9.dev@gmail.com>
2025-11-24 00:47:09 +0900

    Remove dependencies on aws-sdk-swift

--- Package.swift
+++ Package.swift
@@ -43,9 +43,9 @@ let package = Package(
     //        b. pull in the latest main and update crt version as needed
     //        c. submit PR against "aws-iot-device-sdk-swift-testing-branch" for review and merge
     // Once the PRs are merged, you can update the version here for aws-crt-swift, remember to test locally to make sure the Package.swift resolves correctly.
-    .package(
-      url: "https://github.com/awslabs/aws-sdk-swift.git",
-      branch: "aws-iot-device-sdk-swift-testing-branch"),
+    // .package(
+    //   url: "https://github.com/awslabs/aws-sdk-swift.git",
+    //   branch: "aws-iot-device-sdk-swift-testing-branch"),
   ],
   targets: [
     .target(
@@ -79,13 +79,13 @@ let package = Package(
       ],
       path: "ServiceClients/AwsIotJobsClient"
     ),
-    .testTarget(
-      name: "IotJobsClientTests",
-      dependencies: [
-        "IotJobsClient",
-        .product(name: "AWSIoT", package: "aws-sdk-swift"),
-      ],
-      path: "Tests/IotJobsClientTests"),
+    // .testTarget(
+    //   name: "IotJobsClientTests",
+    //   dependencies: [
+    //     "IotJobsClient",
+    //     .product(name: "AWSIoT", package: "aws-sdk-swift"),
+    //   ],
+    //   path: "Tests/IotJobsClientTests"),
     .target(
       name: "IotIdentityClient",
       dependencies: [
@@ -93,12 +93,12 @@ let package = Package(
       ],
       path: "ServiceClients/AwsIotIdentityClient"
     ),
-    .testTarget(
-      name: "IotIdentityClientTests",
-      dependencies: [
-        "IotIdentityClient",
-        .product(name: "AWSIoT", package: "aws-sdk-swift"),
-      ],
+    // .testTarget(
+    //   name: "IotIdentityClientTests",
+    //   dependencies: [
+    //     "IotIdentityClient",
+    //     .product(name: "AWSIoT", package: "aws-sdk-swift"),
+    //   ],
       path: "Tests/IotIdentityClientTests"
     ),
   ]
